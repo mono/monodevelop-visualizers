3faab87d3658e5693b17b6e01de2a02ff1f8cd00
Chris Toshok <toshok@novell.com>
2005-04-15 21:44:50 +0000

    2005-04-15  Chris Toshok  <toshok@ximian.com>
    	* Makefile.am (GLADE_FILES): add stringvis.glade
    	(RESOURCES): same.
    
    	* corlib/stringvis.glade: new file, the string visualizer dialog.
    
    	* corlib/StringVisualizer.cs (StringWindow): display a glade ui
    	containing a textview showing the string.  Paves the way for an
    	editable UI once ReplaceObject is implemented.
    	(StringVisualizer.Show): use StringWindow.
    
    
    svn path=/trunk/monodevelop-visualizers/; revision=43082

--- corlib/StringVisualizer.cs
+++ corlib/StringVisualizer.cs
@@ -1,19 +1,57 @@
 using Gtk;
+using Glade;
+
 using MonoDevelop.DebuggerVisualizers;
 
 namespace CorlibVisualizers
 {
+	public class StringWindow
+	{
+		[Widget] Gtk.Dialog string_dialog;
+		[Widget] Gtk.TextView string_textview;
+		Gtk.TextBuffer buffer;
+	  
+		public StringWindow (string data)
+		{
+			Glade.XML ui;
+
+			buffer = new Gtk.TextBuffer (null);
+
+			ui = Glade.XML.FromAssembly ("stringvis.glade", "string_dialog", null);
+			ui.Autoconnect (this);
+
+			string_dialog.SetDefaultSize (400, 300);
+
+			string_textview.Buffer = buffer;
+			buffer.Text = data;
+		}
+
+		public void Show ()
+		{
+			string_dialog.Run();
+			string_dialog.Hide();
+		}
+
+		public bool Editable {
+			get {
+				return string_textview.Editable;
+			}
+			set {
+				string_textview.Editable = value;
+				string_textview.CursorVisible = value;
+			}
+		}
+	}
+
 	public class StringVisualizer : DialogDebuggerVisualizer
 	{
 		protected override void Show(IDialogVisualizerService windowService, IVisualizerObjectProvider objectProvider)
 		{
-			MessageDialog dialog = new MessageDialog (null, 
-								  DialogFlags.Modal, MessageType.Info, ButtonsType.Close, false,
-								  objectProvider.GetObject().ToString());
-			dialog.Title = "String Visualizer";
+			StringWindow win = new StringWindow (objectProvider.GetObject().ToString());
 
-			dialog.Run();
-			dialog.Hide();
+			win.Editable = objectProvider.IsObjectReplaceable;
+
+			win.Show ();
 		}
 	}
 }
